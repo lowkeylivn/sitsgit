97049ff50319fd05e0ba3d01dc1882cb23fc4c8e
CK <CK@DESKTOP-DRE4TO5>
2017-01-06 21:49:48 -0500

    first commit

---
+++ sitstest/src/main/java/sitstest/sitstest/SITSmercury.java
@@ -0,0 +1,15 @@
+package sitstest.sitstest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SITSmercury {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubri
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://newtours.demoaut.com/");
+		System.out.println("Mercury Page is open");
+	}
+
+}
